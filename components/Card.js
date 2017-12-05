d139e67802eb59383fff05d8667346e13e0f8e1e
Phien Pham <phien@triip.me>
2017-12-05 18:27:10 +0700

    Bug fix on wrong state of showing answer or question

--- components/Card.js
+++ components/Card.js
@@ -6,7 +6,7 @@ import { center, deckCardsNumber, title } from '../styles'
 
 class Card extends Component {
   state = {
-    show: 'answer',
+    show: 'question',
   }
 
   render() {
@@ -16,12 +16,14 @@ class Card extends Component {
       <View style={{flex: 1}}>
         <View style={[styles.center, {flex: 2}]}>
           <Text style={styles.deckCardsNumber}>{`${quiz.currentCard + 1} / ${decks[deckId].questions.length}`}</Text>
+          
           <Text style={[styles.title]}>
             {this.state.show === 'question' ?
               decks[deckId].questions[quiz.currentCard].question :
               decks[deckId].questions[quiz.currentCard].answer
             }
           </Text>
+          
           <Button
             type='white'
             onPress={() => this.setState((prevState) => ({show: prevState.show === 'question' ? 'answer' : 'question'}))}>
